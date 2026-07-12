a6d5022549be1903293bf61f984af4b3dc06054b
Shibin <shibins7862004@gmail.com>
2026-07-12 17:43:32 +0530

    chnaged db from MYSQL to sqlite

--- Backend/todo_project/settings.py
+++ Backend/todo_project/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'todo_db',
-        'USER' : 'root',
-        'PASSWORD' : 'shibin123',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
