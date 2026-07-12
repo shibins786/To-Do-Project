165ec64c89892e8ef87cd70cf250649705c87db3
Shibin <shibins7862004@gmail.com>
2026-07-12 16:51:36 +0530

    Update API base URL for production

--- frontend/todo-frontend/src/api/axios.js
+++ frontend/todo-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL : "http://127.0.0.1:8000/api", //"Whenever you make a request, always start with this URL."
+    baseURL : "https://to-do-project-1dz6.onrender.com/api", //"Whenever you make a request, always start with this URL."
     headers : {
         "Content-Type":"application/json"  //,"Whenever you send a request, also send these headers."
     }
